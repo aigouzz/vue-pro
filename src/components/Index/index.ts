f1095f427e367fd94f8d2b2f4226b07b951c688c
guoxunchao <guoxunchao@xiaomi.com>
2019-06-14 21:37:38 +0800

    修改typescript

--- src/components/Index/index.ts
+++ src/components/Index/index.ts
@@ -21,7 +21,7 @@ export default {
         page_start: self.page,
       };
       Util.get('//movie.douban.com/j/search_subjects', data).then((res:object): any => {
-        window.console.log(res);
+        window.console.log(res); // ok
         // self.list = res.subjects;
       }, (err: object): any => {
         window.alert('接口出错了。。')
